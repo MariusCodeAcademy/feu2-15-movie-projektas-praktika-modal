b84725e70bb1e69edc1e9774f248220ac1c69929
Marius <marius@example.com>
2022-10-28 08:50:21 +0300

    add movie modal open works

---
+++ assets/scripts/app.js
@@ -0,0 +1,22 @@
+'use strict';
+console.log('app.js');
+
+// taikomes ============================================================================
+const els = {
+  addMovieBtn: document.getElementById('add-movie-btn'),
+  addMovieModal: document.getElementById('add-modal'),
+  backdrop: document.getElementById('backdrop'),
+};
+
+console.log('els ===', els);
+
+// EVENT LISTENERS =====================================================================
+// =====================================================================================
+// =====================================================================================
+
+els.addMovieBtn.addEventListener('click', () => {
+  // parodyti modala
+  els.addMovieModal.classList.add('visible');
+  // parodyti backdrop
+  els.backdrop.classList.add('visible');
+});
