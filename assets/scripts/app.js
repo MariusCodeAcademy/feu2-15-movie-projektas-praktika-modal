6952819195153a230dc420b891440994980fa755
Marius <marius@example.com>
2022-10-28 09:28:01 +0300

    add movie form prevent

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,8 @@ const els = {
   addMovieBtn: document.getElementById('add-movie-btn'),
   addMovieModal: document.getElementById('add-modal'),
   backdrop: document.getElementById('backdrop'),
+  cancelMovieBtn: document.querySelector('.btn--passive'),
+  addMovieForm: document.getElementById('add-movie-form'),
 };
 
 console.log('els ===', els);
@@ -23,12 +25,26 @@ els.addMovieBtn.addEventListener('click', () => {
 });
 
 // uzdeti paspaudimo pasiklausyma ant backdrop
-els.backdrop.addEventListener('click', () => {
-  console.log('backdrop click detected');
+els.backdrop.addEventListener('click', closeMovieModal);
+
+// uzdeti cancel mygtukui pasiklausyma ir atlikti ta pati ka ir darem su backdrop paspaudus
+els.cancelMovieBtn.addEventListener('click', closeMovieModal);
+
+// klausomes formos issiuntimo ir stabdom perkrovima
+els.addMovieForm.addEventListener('submit', (event) => {
+  // stabdom perkrovima
+  event.preventDefault();
+  console.log('add movie');
+});
+
+// MAIN FUNCTIONS =====================================================================
+// =====================================================================================
+// =====================================================================================
+
+function closeMovieModal() {
+  console.log('closeMovieModal fn');
   // paslepti modala
   els.addMovieModal.classList.remove('visible');
   // paslepti backdrop
   els.backdrop.classList.remove('visible');
-});
-
-// uzdeti cancel mygtukui pasiklausyma ir atlikti ta pati ka ir darem su backdrop paspaudus
+}
