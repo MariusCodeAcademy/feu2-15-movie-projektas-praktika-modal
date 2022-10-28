5a1ec3c5cd2b445240c94074254bf7999bad47b8
Marius <marius@example.com>
2022-10-28 09:01:29 +0300

    close modal

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -14,9 +14,21 @@ console.log('els ===', els);
 // =====================================================================================
 // =====================================================================================
 
+// paspaudimas ant Add Movie Btn
 els.addMovieBtn.addEventListener('click', () => {
   // parodyti modala
   els.addMovieModal.classList.add('visible');
   // parodyti backdrop
   els.backdrop.classList.add('visible');
 });
+
+// uzdeti paspaudimo pasiklausyma ant backdrop
+els.backdrop.addEventListener('click', () => {
+  console.log('backdrop click detected');
+  // paslepti modala
+  els.addMovieModal.classList.remove('visible');
+  // paslepti backdrop
+  els.backdrop.classList.remove('visible');
+});
+
+// uzdeti cancel mygtukui pasiklausyma ir atlikti ta pati ka ir darem su backdrop paspaudus
